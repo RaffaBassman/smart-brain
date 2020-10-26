1baa4f8318b2edb3b9bd66ab7e87b984fdbb4056
RaffaBassman <raffabassman@users.noreply.github.com>
2020-10-26 09:17:16 +0100

    changing localhost to heroku

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://immense-cove-01806.herokuapp.com/imageurl", {
       method: "post",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends React.Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch("http://localhost:3000/image", {
+        fetch("https://immense-cove-01806.herokuapp.com/image", {
           method: "put",
           headers: {"Content-Type": "application/json"},
           body: JSON.stringify({
